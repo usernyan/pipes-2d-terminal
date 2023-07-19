06109b7f9ddc5e7362146d0a216c4ad92bec8c9d
usernyan <74884883+usernyan@users.noreply.github.com>
2023-07-18 21:01:11 -0400

    added color support

--- pipes2d.c
+++ pipes2d.c
@@ -38,8 +38,10 @@ struct trailer {
     struct vec pos;
     struct vec dir;
     struct vec prev_dir;
+    int color_pair_idx;
 };
 
+bool term_has_colors; //GLOBAL
 int max_x, max_y; //GLOBAL
 void move_trailer(struct trailer *t) {
     wchar_t box_draws[3][3] =
@@ -54,6 +56,9 @@ void move_trailer(struct trailer *t) {
         int turn_dir = ((double) (random_choice) / 2 - 0.25) * 4;
         t->dir = turn(t->dir, turn_dir);
     }
+    if (term_has_colors) {
+        attron(COLOR_PAIR(t->color_pair_idx));
+    }
     if (t->prev_dir.x != t->dir.x && t->prev_dir.x != -t->dir.x) {
         //set the previous character to the appropriate corner piece
         struct vec sussy_diff = vec_diff(t->prev_dir, t->dir);
@@ -64,7 +69,10 @@ void move_trailer(struct trailer *t) {
     else
         mvaddch(t->pos.x, t->pos.y, ACS_VLINE);
     t->prev_dir = t->dir;
- 
+     if (term_has_colors) {
+        attroff(COLOR_PAIR(t->color_pair_idx));
+    }
+
     t->pos = vec_add(t->pos, t->dir);
     if (t->pos.x < 0) {
         t->pos.x = max_x - 1;
@@ -79,6 +87,17 @@ void move_trailer(struct trailer *t) {
 int main() {
     setlocale(LC_CTYPE, "");
     WINDOW *W = initscr();
+    term_has_colors = has_colors();
+    int num_col_pairs = 5;
+    if (term_has_colors) {
+        start_color();
+        use_default_colors();
+        init_pair(1, COLOR_WHITE, COLOR_BLACK);
+        init_pair(2, COLOR_CYAN, COLOR_BLACK);
+        init_pair(3, COLOR_MAGENTA, COLOR_BLACK);
+        init_pair(4, COLOR_RED, COLOR_BLACK);
+        init_pair(5, COLOR_YELLOW, COLOR_BLACK);
+    }
     cbreak();
     noecho();
     intrflush(W, FALSE);
@@ -92,9 +111,11 @@ int main() {
    
     struct trailer all_trailers[5];
     for (size_t i = 0; i < sizeof(all_trailers) / sizeof(struct trailer); i++) {
-        all_trailers[i].pos = (struct vec){0, 0};
-        all_trailers[i].dir = (struct vec){0, 1};
-        all_trailers[i].prev_dir = all_trailers[i].dir;
+        struct trailer *t = &all_trailers[i];
+        t->pos = (struct vec){0, 0};
+        t->dir = (struct vec){0, 1};
+        t->prev_dir = t->dir;
+        t->color_pair_idx = i % num_col_pairs + 1;
     }
     srand(12);
     while(true) {
